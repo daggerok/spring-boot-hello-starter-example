b7e97d5164a2a06879607481f7dddfbe589117c8
Maksim Kostromin <daggerok@gmail.com>
2018-06-27 15:15:24 +0300

    I should get a raise for this. (C) whatthecommit.com

--- hello-service-autoconfigure/src/main/java/com/github/daggerok/hello/config/props/HelloProperties.java
+++ hello-service-autoconfigure/src/main/java/com/github/daggerok/hello/config/props/HelloProperties.java
@@ -4,10 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @NoArgsConstructor
 @Accessors(chain = true)
 @ConfigurationProperties("com.github.daggerok")
