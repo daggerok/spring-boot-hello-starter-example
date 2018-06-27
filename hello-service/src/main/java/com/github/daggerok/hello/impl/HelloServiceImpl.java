d4748dbd6b5d0c2ca16f24209643668198772882
Maksim Kostromin <daggerok@gmail.com>
2018-06-27 13:58:31 +0300

    First fix | this is my quickfix branch and i will use to do my quickfixes (C) whatthecommit.com

--- hello-service/src/main/java/com/github/daggerok/hello/impl/HelloServiceImpl.java
+++ hello-service/src/main/java/com/github/daggerok/hello/impl/HelloServiceImpl.java
@@ -2,12 +2,14 @@ package com.github.daggerok.hello.impl;
 
 import com.github.daggerok.hello.HelloService;
 
+import static java.lang.String.format;
+
 //tag::content[]
 public class HelloServiceImpl implements HelloService {
 
   @Override
   public String sayHello(String whom) {
-    return null;
+    return format("Hello, %s!", whom);
   }
 }
 //end::content[]
--- spring-boot-starter-hello-tests/src/main/java/com/github/daggerok/tests/HelloStarterTestApplication.java
+++ spring-boot-starter-hello-tests/src/main/java/com/github/daggerok/tests/HelloStarterTestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.stream.Stream;
+
 //tag::content[]
 @Log4j2
 @SpringBootApplication
