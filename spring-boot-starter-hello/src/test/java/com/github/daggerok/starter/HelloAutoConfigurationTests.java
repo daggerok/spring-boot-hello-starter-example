feb49509dc78a4f5d6f186eb637ebd8e5a7dc2f9
Maksim Kostromin <daggerok@gmail.com>
2018-06-27 11:33:57 +0300

    touched... (C) whatthecommit.com

--- spring-boot-starter-hello/src/test/java/com/github/daggerok/starter/HelloAutoConfigurationTests.java
+++ spring-boot-starter-hello/src/test/java/com/github/daggerok/starter/HelloAutoConfigurationTests.java
@@ -1,7 +1,11 @@
 package com.github.daggerok.starter;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.rule.OutputCapture;
+
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
 //
@@ -9,6 +13,12 @@ import org.junit.runner.RunWith;
 //@SpringBootTest(classes = HelloAutoConfiguration.class,
 //    webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class HelloAutoConfigurationTests {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Rule
+  public OutputCapture outputCapture = new OutputCapture();
 
   @Test
   public void contextLoads() { }
